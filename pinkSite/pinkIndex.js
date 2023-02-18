e5c9e1cf95c52b73062c574b406ad716faf9a2c8
Arina Stepanova <mrs.arina.stepanova@gmail.com>
2023-02-18 17:07:17 +0600

    added pink site project

---
+++ pinkSite/pinkIndex.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    $(".toggle").on('click', function(){
+
+        if($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        }
+        else {
+            $(".item").addClass("active");
+
+        }
+    })
+});
